a813f3f940fa36ab4bff16898dff73bfbca13685
Nigel Nkomo <mrnbnkomo@gmail.com>
2023-03-05 19:38:36 +0100

    add dependencies to the useEffect hook

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import VideoPlayer from "./VideoPlayer";
 import "./App.css";
 
 function App() {
+  //state
   const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState("");
 
   return (
-    <div className="container">
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? <PauseRoundedIcon /> : <PlayArrowRoundedIcon />}
-      </button>
+    <>
+      <div className="container">
+        <h1>Synchronising with effects </h1>
 
-      <VideoPlayer
-        src="https://joy1.videvo.net/videvo_files/video/free/2012-07/small_watermarked/Transit%20of%20Venus_preview.webm"
-        isPlaying={isPlaying}
-      />
-    </div>
+        {/* The input updates the parent component's state. Typing causes useEffect to rerun because without the dependencies array, useEffect reruns after every rerender. */}
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        {/* Video player element */}
+        <VideoPlayer
+          src="https://joy1.videvo.net/videvo_files/video/free/video0471/small_watermarked/_import_618a60f421e389.11686272_preview.webm"
+          isPlaying={isPlaying}
+        />
+
+        {/* Button */}
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? (
+            <PauseRoundedIcon viewBox="0 0 24 24" />
+          ) : (
+            <PlayArrowRoundedIcon viewBox="0 0 24 24" />
+          )}
+        </button>
+      </div>
+    </>
   );
 }
 
--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -3,17 +3,23 @@ import { useEffect, useRef } from "react";
 export default function VideoPlayer({ src, isPlaying }) {
   const ref = useRef();
 
+  // synchronise isPlaying with play() and pause() i.e. when the value of isPlaying changes, change the method (play() or pause())
+  //specify the dependencies array
+  //the code inside useEffect depends on the value of isPlaying
+  //if isPlaying's value is the same, useEffect won't rerun
   useEffect(() => {
     if (isPlaying) {
+      console.log("calling video.play()");
       ref.current.play();
     } else {
+      console.log("calling video.pause()");
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
 
   return (
     <>
-      <video ref={ref} src={src} loop playsInline />
+      <video ref={ref} src={src} loop playsInline muted />
     </>
   );
 }
